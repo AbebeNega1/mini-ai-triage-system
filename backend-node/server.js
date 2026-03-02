7dd1be65edf5b932304fb3363d4175d558768cd7
Abebe Nega <your_email@example.com>
2026-03-02 23:42:23 +0300

    Fix DB connection using MYSQL_URL

--- backend-node/server.js
+++ backend-node/server.js
@@ -12,18 +12,13 @@ app.use(express.json());
 // ===============================
 // MySQL Connection
 // ===============================
-const db = mysql.createConnection({
-  host: process.env.MYSQLHOST || process.env.DB_HOST,
-  user: process.env.MYSQLUSER || process.env.DB_USER,
-  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
-  database: process.env.MYSQLDATABASE || process.env.DB_NAME,
-  port: process.env.MYSQLPORT || 3306,
-});
+const db = mysql.createConnection(process.env.MYSQL_URL);
+
 db.connect((err) => {
   if (err) {
     console.error("Database connection failed:", err);
   } else {
-    console.log("Connected to MySQL database");
+    console.log("Connected to Railway MySQL");
   }
 });
 const createTableQuery = `
